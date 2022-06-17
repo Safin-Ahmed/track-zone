f8ab563e409a132603be3050ed71d3c4229db512
Safin Ahmed <safin.ahmed2000@gmail.com>
2022-06-18 01:36:49 +0600

    Implemented 12 hour format

--- src/components/Clock/Time.jsx
+++ src/components/Clock/Time.jsx
@@ -4,7 +4,11 @@ import classes from "./Time.module.css";
 const Time = ({ hours, minutes, period, seconds }) => {
   return (
     <div className={classes.time}>
-      <span className={classes.hour}>{hours}</span>
+      <span className={classes.hour}>
+        {hours > 12
+          ? (hours - 12).toString().padStart(2, 0)
+          : hours.padStart(2, 0)}
+      </span>
       <span className={classes.dots}>: </span>
       <span className={classes.minutes}>{minutes}</span>
       <div className={classes.rightSide}>
